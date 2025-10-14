68b2ca13acb7162808efbbf8cdd9dc28292e467d
SoniaSGaming <112811426+SoniaSGaming@users.noreply.github.com>
2025-10-14 09:11:38 +0300

    Update surface.h

--- engine/surface.h
+++ engine/surface.h
@@ -2,22 +2,20 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <utility>
 
 #ifndef _SURFACE_H_
 #define _SURFACE_H_
 
 class Surface {
     public:
-        int x;
-        int y;
-        int width;
-        int height;
+        std::pair<int, int> size;
         std::vector<std::string> graphic;
         int color=7;
 
         Surface(int wi, int hi, char sym) {
-            width=wi;
-            height=hi;
+            size.first = wi;
+            size.second = hi;
 
             for(int i=0;i<hi;++i)
             {
